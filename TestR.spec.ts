4a36c2baaf347bed0ce1d30bda4412126d889feb
prafull059 <127289358+prafull059@users.noreply.github.com>
2023-04-17 16:18:43 +0530

    Delete TestR.spec.ts

--- TestR.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('has title', async ({ page }) => {
-
-    await page.goto("https://www.quiklyz.com/")
-    await page.screenshot({ path: './screenshot.png', fullPage: true });
-})
